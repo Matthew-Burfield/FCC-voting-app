5b099cd9abdc6a136c84a32d3958dcdff729777a
Matthew-Burfield <m1.burfield@gmail.com>
2017-08-26 08:26:00 +1000

    initial commit. Connects to mongo, authentication on survey route with auth0

---
+++ server/server.js
@@ -0,0 +1,128 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const jwt = require('express-jwt')
+const moment = require('moment')
+const mongodb = require('mongodb')
+const request = require('request')
+const rsaValidation = require('auth0-api-jwt-rsa-validation')
+
+const app = express()
+const mongoUri = process.env.MONGO_URL
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const returnError = (res, err) => {
+	res.json({
+		success: false,
+		error: err,
+	})
+}
+
+const jwtCheck = jwt({
+	secret: rsaValidation(),
+	algorithms: ['RS256'],
+	issuer: process.env.ISSUER,
+	audience: process.env.AUDIENCE,
+})
+
+app.use(function (err, req, res, next) {
+	if (err.name === 'UnauthorizedError') {
+		res.status(401).json({ message: 'Missing or invalid token' })
+	}
+})
+
+app.get('/surveys', function(req, res) {
+	mongodb.MongoClient.connect(mongoUri, function(err, db) {
+		if (err) throw err
+		db.collection('survey').find({}).toArray(function(err, result) {
+			res.json(result)
+			db.close()
+		})
+	})
+})
+
+
+app.post('/vote', urlencodedParser, function(req, res) {
+	const id = req.body.surveyId
+	const vote = `pollOptions.${req.body.pollOptionIndex}.votes`
+	mongodb.MongoClient.connect(mongoUri, function(err, db) {
+		if (err) returnError(res, err)
+		db.collection('survey').update(
+			{ _id: mongodb.ObjectID(id) },
+			{ $inc: { [vote]: 1 }}, function(err, result) {
+			if (err) returnError(res, err)
+			db.close()
+			res.json({
+				success: true,
+			})
+		})
+	})
+})
+
+app.post('/addPollOption', jwtCheck, urlencodedParser, function(req, res) {
+	const id = req.body.surveyId
+	const pollOption = {
+		title: req.body.title,
+		votes: 0,
+	}
+	mongodb.MongoClient.connect(mongoUri, function(err, db) {
+		if (err) returnError(res, err)
+		db.collection('survey').update(
+			{ _id: mongodb.ObjectID(id) },
+			{ $push: { pollOptions: pollOption } }, function(err, result) {
+			if (err) returnError(res, err)
+			db.close()
+			res.json({
+				success: true,
+			})
+		})
+	})
+})
+
+app.post('/comment', jwtCheck, urlencodedParser, function(req, res) {
+	const id = req.body.surveyId
+	const comment = {
+		value: req.body.comment,
+		datetime: moment().valueOf(),
+	}
+	mongodb.MongoClient.connect(mongoUri, function(err, db) {
+		if (err) returnError(res, err)
+		db.collection('survey').update(
+			{ _id: mongodb.ObjectID(id) },
+			{ $push: { comments: comment }}, function(err, result) {
+			if (err) returnError(res, err)
+			db.close()
+			res.json({
+				success: true,
+			})
+		})
+	})
+})
+
+app.post('/survey', jwtCheck, urlencodedParser, function(req, res) {
+	const survey = {
+		title: req.body.title,
+		isPublished: req.body.isPublished,
+		pollOptions: req.body.pollOption.map(value => ({
+			title: value,
+			votes: 0,
+		})),
+		isDeleted: 0,
+		comments: [],
+	}
+	mongodb.MongoClient.connect(mongoUri, function(err, db) {
+		if (err) returnError(res, err)
+		db.collection('survey').insertOne(survey, function(err, result) {
+			if (err) returnError(res, err)
+			db.close()
+			res.json({
+				success: true,
+				survey,
+			})
+		})
+	})
+})
+
+app.listen(8000, function () {
+	console.log('Example app listening on port 8000!')
+})
