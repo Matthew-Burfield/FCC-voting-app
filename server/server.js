80054a3b905f2dee09926ec7fc3c813bb67b45b9
Matthew-Burfield <m1.burfield@gmail.com>
2017-10-14 10:34:48 +1000

    added usersVoted array to survey

--- server/server.js
+++ server/server.js
@@ -134,16 +134,20 @@ app.post('/comment', jwtCheck, unauthorized, function(req, res) {
 })
 
 app.post('/survey', jwtCheck, unauthorized, function(req, res) {
-	const survey = {
-		title: req.body.title,
-		isPublished: req.body.isPublished,
+	const surveyDefaults = {
+		comments: [],
 		datetime: moment().valueOf(),
+		isDeleted: 0,
+		usersVoted: [],
+	}
+	const survey = {
+		isPublished: req.body.isPublished,
 		pollOptions: req.body.pollOptions.map(value => ({
 			title: value,
 			votes: 0,
 		})),
-		isDeleted: 0,
-		comments: [],
+		title: req.body.title,
+		...surveyDefaults,
 	}
 	mongodb.MongoClient.connect(mongoUri, function(err, db) {
 		if (err) returnError(res, err)
