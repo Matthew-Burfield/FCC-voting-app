089457563034237403090b93fb8c8f8df0934453
Matthew-Burfield <m1.burfield@gmail.com>
2017-12-18 18:15:34 +1000

    updated API domains

--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -1,10 +1,10 @@
-export const API_DOMAIN = 'http://localhost:8000';
+export const API_DOMAIN = 'https://burfield-voting-app.herokuapp.com/';
 export const authDomain = 'matthew-burfield.au.auth0.com'
 export const audience = 'matthew-burfield.com.au/voting-app'
 export const scope = 'openid%20profile%20email'
 export const responseType = 'id_token%20token'
 export const clientId = '9a0Bi5RDIAHWs8j3hwGJm8EEPR17IIGE'
-export const redirectUrl = 'http://localhost:3000/login'
+export const redirectUrl = 'https://matthew-burfield.github.io/FCC-voting-app/login'
 export const nonce = '123abc'
 export const state = '123abc'
 export const noSurvey = { notFound: 'notFound' }
